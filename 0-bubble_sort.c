edb5578e071ee1b8e8b1d21c1333667b42c4f8d0
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-23 18:19:31 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,16 +6,17 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	long unsigned int i, swapped = 1, temp;
-	while(swapped)
+	unsigned long int i, swapped = 1, temp;
+
+	while (swapped)
 	{
 		swapped = 0;
-		for(i = 1; i < size; i++)
+		for (i = 1; i < size; i++)
 		{
 			if (array[i - 1] > array[i])
 			{
-				temp = array[i-1];
-				array[i-1] = array[i];
+				temp = array[i - 1];
+				array[i - 1] = array[i];
 				array[i] = temp;
 				print_array(array, size);
 				swapped = 1;
