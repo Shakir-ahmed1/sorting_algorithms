9784a10fa75d9da570e19ac0b59dd7bc9a042d80
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-23 18:17:14 +0000

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ * bubble_sort - a sorting mechanism
+ * @array: the array of integers
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	long unsigned int i, swapped = 1, temp;
+	while(swapped)
+	{
+		swapped = 0;
+		for(i = 1; i < size; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				temp = array[i-1];
+				array[i-1] = array[i];
+				array[i] = temp;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
 #ifndef _SORT_H_
 #define _SORT_H_
-
+#include<stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,5 +15,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-
+void print_array(const int *, size_t);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 #endif
