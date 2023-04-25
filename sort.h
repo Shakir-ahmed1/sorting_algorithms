e708464923ca029e6fc5b5641621828f550ac87e
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-25 22:34:46 +0000

    updating the header files

--- sort.h
+++ sort.h
@@ -22,7 +22,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void cocktail_sort(listint_t **list);
+void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
